924b099983063c2f86bb97749a06bf160873c810
TikGuan <Tik.Guan@walmart.com>
2022-01-18 14:00:33 +0800

    改为history路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
@@ -16,8 +16,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(), // createWebHashHistory()
   routes,
-})
+});
 
 export default router;
