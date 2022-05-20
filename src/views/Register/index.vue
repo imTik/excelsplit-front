40e9b16cbc3e09bf8702b9be7ce56a1026bf96f7
TikGuan <Tik.Guan@walmart.com>
2022-05-20 11:18:51 +0800

    增加模拟点击事件

--- src/views/Register/index.vue
+++ src/views/Register/index.vue
@@ -1,147 +1,69 @@
 <!--  -->
 <template>
-  <div class="register">
-    <componentLists></componentLists>
+  <div class="register" @click="globalClick" ref="global">
+    <p>测试XY值点击</p>
 
-    <div
-      @drop="drop($event)"
-      @dragover="dragover($event)"
-      class="register-main"
-    >
-      <template v-for="(component, index) in pageData" :key="index">
-        <div
-          class="component-box"
-          v-if="component.type === 'button'"
-          draggable="true"
-          :data-index="index"
-          @dragstart="moveComponent($event)"
-        >
-          <span class="del" @click="deleteCompinent(index)">删除</span>
-          <NButton>{{ component.text }}</NButton>
-        </div>
-
-        <div
-          class="component-box"
-          v-else-if="component.type === 'input'"
-          draggable="true"
-          :data-index="index"
-          @dragstart="moveComponent($event)"
-        >
-          <span class="del" @click="deleteCompinent(index)">删除</span>
-          <NInput :placeholder="component.text"></NInput>
-        </div>
-
-        <div
-          class="component-box"
-          v-else-if="component.type === 'checkbox'"
-          draggable="true"
-          :data-index="index"
-          @dragstart="moveComponent($event)"
-        >
-          <span class="del" @click="deleteCompinent(index)">删除</span>
-          <NCheckboxGroup v-model:value="component.value">
-            <NCheckbox
-              v-for="(item, iIndex) in component.items"
-              :key="iIndex"
-              :value="item.value"
-              >{{ item.label }}</NCheckbox
-            >
-          </NCheckboxGroup>
-        </div>
-
-        <div
-          class="component-box"
-          v-else-if="component.type === 'radio'"
-          draggable="true"
-          :data-index="index"
-          @dragstart="moveComponent($event)"
-        >
-          <span class="del" @click="deleteCompinent(index)">删除</span>
-          <NRadio>{{ component.text }}</NRadio>
-        </div>
-
-        <div
-          class="component-box"
-          v-else-if="component.type === 'select'"
-          draggable="true"
-          :data-index="index"
-          @dragstart="moveComponent($event)"
-        >
-          <span class="del" @click="deleteCompinent(index)">删除</span>
-          <NSelect :placeholder="component.text"></NSelect>
-        </div>
-      </template>
-    </div>
+    <span
+      class="square"
+      v-for="(squareIndex, index) in totalSquare"
+      :key="index"
+      :data-index="index"
+    ></span>
   </div>
 </template>
 
 <script setup>
-import {
-  NButton,
-  NInput,
-  NCheckboxGroup,
-  NCheckbox,
-  NRadioGroup,
-  NRadio,
-  NSelect,
-} from "naive-ui";
-import { ref, reactive } from "vue";
-import { componentFactory } from "./componentFactory";
-import componentLists from "./components/compoentLists.vue";
+import { NButton } from "naive-ui";
+import { ref, reactive, onMounted } from "vue";
 
-let pageData = ref([]);
-function drop(e) {
-  let type = e.dataTransfer.getData("type");
-  let moveIndex = e.dataTransfer.getData("index");
-  let dropIndex = whileFindIndex(e);
+let totalSquare = ref(3000);
+const global = ref("child");
 
-  if (type) {
-    addComponent(type);
-    type = null;
-    console.log(pageData.value);
-    let len = pageData.value.length;
-    pageData.value[len-1].getType();
-  }
-
-  if (moveIndex && dropIndex) {
-    transpositionCompoent(dropIndex, moveIndex);
-  }
+function globalClick(e) {
+  console.log(e.clientX, e.clientY);
+  console.log("global click: ", e.target);
 }
 
-function addComponent(type) {
-  let _component = componentFactory(type);
-  pageData.value.push(_component);
-  type = null;
+function createSimulateClick() {
+  let _event = null;
+  return function (X, Y) {
+    console.log("-----sumulate start-----");
+    if (!_event) {
+      console.log('首次创建MouseEvent事件');
+      _event = document.createEvent("MouseEvents");
+    };
+    /**
+     * typeArg - 指定事件类型。
+     * canBubbleArg - 指定该事件是否可以 bubble。
+     * cancelableArg - 指定是否可以阻止事件的默认操作
+     * viewArg - 指定  Event 的  AbstractView。
+     * detailArg - 指定  Event 的鼠标单击量。
+     * screenXArg - 指定  Event 的屏幕 x 坐标
+     * screenYArg - 指定  Event 的屏幕 y 坐标
+     * clientXArg - 指定  Event 的客户机 x 坐标
+     * clientYArg - 指定  Event 的客户机 y 坐标
+     * ctrlKeyArg - 指定是否在  Event 期间按下 control 键。
+     * altKeyArg - 指定是否在  Event 期间按下 alt 键。
+     * shiftKeyArg - 指定是否在  Event 期间按下 shift 键。
+     * metaKeyArg - 指定是否在  Event 期间按下 meta 键。
+     * buttonArg - 指定  Event 的鼠标按键。
+     * relatedTargetArg - 指定  Event 的相关  EventTarget。
+     */
+    _event.initMouseEvent("click", false, false, window, 2, 0, 0, X, Y, false, false, false, false, 1, null);
+    global.value.dispatchEvent(_event);
+    console.log("-----sumulate end-----\n\n\n");
+  };
 }
 
-function transpositionCompoent(drop, move) {
-  let _moveComponent = pageData.value.splice(drop, 1, pageData.value[move]);
-  pageData.value.splice(move, 1, _moveComponent[0]);
-}
+let simulateClick = createSimulateClick();
 
-function whileFindIndex(e) {
-  const parentEl = e.currentTarget;
-  let el = e.target;
-  let _index = el.getAttribute("data-index");
-  while (!_index && el.tagName !== "BODY") {
-    el = el.parentNode;
-    _index = el.getAttribute("data-index");
-  }
-  return _index;
-}
+onMounted(() => {
+  simulateClick(80, 20);
 
-function dragover(e) {
-  e.preventDefault();
-}
-
-function deleteCompinent(index) {
-  pageData.value.splice(index, 1);
-}
-
-function moveComponent(e) {
-  let index = e.target.getAttribute("data-index");
-  e.dataTransfer.setData("index", index);
-}
+  setTimeout(() => {
+    simulateClick(200, 460);
+  }, 1000);
+});
 </script>
 
 <style lang='less' scoped>
@@ -149,41 +71,13 @@ function moveComponent(e) {
 .register {
   position: relative;
   height: 100%;
-  .register-main {
-    position: relative;
-    height: 100%;
-    margin-left: 300px;
-    margin-right: 400px;
-    padding: 10px;
-    box-sizing: border-box;
-    overflow: auto;
 
-    .component-box {
-      position: relative;
-      padding: 8px;
-      margin-bottom: 2px;
-      box-sizing: border-box;
-      border: 1px solid @line;
-      transition: all 0.2s;
-      &:hover {
-        border-color: @green;
-        .move,
-        .del {
-          display: inline-block;
-        }
-      }
-
-      .del {
-        display: none;
-        position: absolute;
-        right: 10px;
-        top: 5px;
-        font-size: 12px;
-        color: @green;
-        cursor: pointer;
-        z-index: 99;
-      }
-    }
+  .square {
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    margin: 1px;
+    border: 1px solid @line;
   }
 }
 </style>
